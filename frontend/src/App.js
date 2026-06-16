fa0fc4cf6380641f3f06f8a543999b4434c90abd
blurhack <blurhack2332@gmail.com>
2026-06-17 03:18:04 +0530

    Fix router context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import MaterialsPage from "./pages/MaterialsPage";
+import ConstructionPage from "./pages/ConstructionPage";
 
-function App() {
-  return <HomePage />;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/materials" element={<MaterialsPage />} />
+        <Route path="/construction" element={<ConstructionPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
