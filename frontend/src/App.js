7403a82aa4d4c2d9b44000e59ca6033c99cfffa2
yajanmusaligari <yajanreddymusaligari@gmail.com>
2026-06-17 03:25:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import MaterialsPage from './pages/MaterialsPage';
+import ConstructionPage from './pages/ConstructionPage';
+import './App.css';
 
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import MaterialsPage from "./pages/MaterialsPage";
-import ConstructionPage from "./pages/ConstructionPage";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/materials" element={<MaterialsPage />} />
-        <Route path="/construction" element={<ConstructionPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/materials" element={<MaterialsPage />} />
+            <Route path="/construction" element={<ConstructionPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
+export default App;
